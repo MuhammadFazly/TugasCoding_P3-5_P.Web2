314a5fd3d510453aff223a5a796e1b8bb3e9d189
MuhammadFazly <148175980+MuhammadFazly@users.noreply.github.com>
2023-10-23 21:50:09 +0700

    Add files via upload

---
+++ P3latihan2.php
@@ -0,0 +1,15 @@
+<?php
+
+$x = 10;
+
+echo "Aku adalah angka $x </br>";
+$hasil1 = $x * 8;
+echo "Jika aku dikali 8, jumlahku sekarang $hasil1 </br>";
+$hasil2 = $hasil1 / 4;
+echo "Jika aku dibagi 4, jumlahku sekarang $hasil2 </br>";
+$hasil3 = $hasil2 - 6;
+echo "Jika aku dikurang 6, jumlahku sekarang $hasil3</br>";
+$hasil4 = $hasil3 + 2;
+echo "Jika aku ditambah 2 jumlahku sekarang $hasil4</br>";
+
+?>
---
+++ P3latihan3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <link rel="stylesheet" href="lagiin.css">
+  <title>Tabel di HTML</title>
+  <style>
+    table, th, td { border: 1px solid; }
+    width
+  </style>
+ </head>
+<body>
+  <p>Contoh tabel</p>
+  <table>
+    <tr>
+        <td>A</td>
+        
+        
+       
+    </tr>
+    <tr>
+        <td>A</td>
+        <td>B</td>
+        
+       
+    </tr>
+    <tr>
+        <td>A</td>
+        <td>B</td>
+        <td>C</td>
+        
+    </tr>
+  </table>
+</body>
+</html>
